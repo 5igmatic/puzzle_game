2d91d4551b6b0f9322d71c4e579a7d20cb3db73b
Riley O'Brien <64238678+5igmatic@users.noreply.github.com>
2022-02-04 11:55:11 +1000

    Add files via upload

--- Visualiser/main.py
+++ Visualiser/main.py
@@ -1,28 +1,38 @@
+from tkinter import Widget
 import pygame
 
-WIDTH, HEIGHT = 700, 600
+WIDTH, HEIGHT = 700, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
+#alter the dimensions of the collider and starting position for different results
+collider = pygame.Surface((WIDTH, 100))
+collider.fill('white')
+collider_rect = collider.get_rect(topleft = (0, 500))
+WIN.blit(collider, collider_rect)
 
-vertical_line = pygame.Surface((1, 600))
+vertical_line = pygame.Surface((1, HEIGHT))
 vertical_line.fill('white')
 for i in range(8):
     WIN.blit(vertical_line, (100*i, 0))
 
-horizontal_line = pygame.Surface((700, 1))
+horizontal_line = pygame.Surface((WIDTH, 1))
 horizontal_line.fill('white')
-for i in range(7):
+for i in range(8):
     WIN.blit(horizontal_line, (0, 100*i))
 
-for i in range(801):
+for i in range(1001):
     x = i/200
     y = -(2/3)*(x*x-4*x)
     character = pygame.Surface((100, 100)).convert_alpha()
-    intensity = i/3.14
+    intensity = i/4
     colour = (255, intensity, 255-intensity)
     character.fill(colour)
-    character = pygame.transform.rotate(character, -x*90)
+    character = pygame.transform.rotate(character, -x*90%360)
     character_rect = character.get_rect(center = (100*x+150, 500-100*y-50))
+
+    if character_rect.colliderect(collider_rect):
+        break
+
     WIN.blit(character, character_rect)
     pygame.display.update()
 
